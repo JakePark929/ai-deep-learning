4b3bcad1ffe20e1db9c4a717d83b5a9cf21814f3
epike <epikeran@gmail.com>
2023-07-28 08:28:10 +0900

    init: Initialize ai deepleaning with python project

---
+++ ch01_pythonbasic/ch01_print.py
@@ -0,0 +1,15 @@
+a = 10
+
+# print("Hello World")
+print("\n=== 0. Hello Python ===")
+print(a)
+
+# 안녕 파이썬
+print("\n=== 1. print ===")
+print(1, 10, 100, sep="*", end="!!\n")
+
+print("\n=== 2. variable ===")
+a = 9
+a = a - 3
+
+print(a)
