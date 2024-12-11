cde8bfd43719e0cfbb734a30382604c73724b770
jake <epikeran@gmail.com>
2024-12-11 13:55:41 +0900

    feat : Add text splitter basic code

--- part13_invest_assist/02_pypdf_parsing.py
+++ part13_invest_assist/02_pypdf_parsing.py
@@ -3,8 +3,7 @@ from langchain_community.document_loaders import PyPDFLoader
 loader = PyPDFLoader("./src/bok_sample.pdf")
 pages = loader.load_and_split()
 
-text_2 = pages[2].page_content
-text_3 = pages[3].page_content
+text = pages[0].page_content
 
-print(text_2)
-print(text_3)
+print(len(pages))
+print(text)
