0a0e5e821a34380eb723c5488fdb20379ceda701
jake <epikeran@gmail.com>
2024-12-20 17:28:03 +0900

    feat : Add answer logic with gpt

--- part9_ai_faq/search_and_answer.py
+++ part9_ai_faq/search_and_answer.py
@@ -1,11 +1,16 @@
 import os
+
 from dotenv import load_dotenv
+from openai import Client
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+
+from prompt_template import prompt_template
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 INDEX_PATH = os.getenv("INDEX_PATH")
+client = Client(api_key=OPENAI_API_KEY)
 directory = INDEX_PATH
 
 def search(question):
@@ -23,8 +28,29 @@ def search(question):
 
     return result[0].metadata
 
+def generate_answer(context, question):
+    context_join = f"""Q: {context['question']}
+A: {context['answer']}"""
+    prompt = prompt_template.format(context=context_join, question=question)
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0,
+    )
+
+    output = response.choices[0].message.content
+
+    return output
+
 if __name__ == "__main__":
     question = "페이백은 어디서 받나요?"
     qa = search(question)
     print(qa['question'])
     print(qa['answer'])
+    print("=====")
+    print(question)
+    print(generate_answer(qa, question))
