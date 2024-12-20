f4eb3d776c4f0dd25e56fed7a3c1952170ff7985
jake <epikeran@gmail.com>
2024-12-20 10:22:20 +0900

    feat : Integrate movie review analysis with langchain

--- part6_naver_review/ch05/inference.py
+++ part6_naver_review/ch05/inference.py
@@ -2,8 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from openai import Client
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-from prompt_template import prompt_template
+from prompt_template import prompt_template, prompt_template_langchain
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -23,9 +26,27 @@ def inference_all(reviews):
     output = response.choices[0].message.content
 
     return output
+
+prompt = PromptTemplate(
+    template=prompt_template_langchain,
+    input_variables=["reviews"]
+)
+model = ChatOpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model_name="gpt-3.5-turbo",
+    temperature=0.0
+)
+output_parser = StrOutputParser()
+chain = (prompt | model | output_parser)
+
+def inference_all_langchain(reviews):
+    reviews = "\n".join([f"review_no: {review['id']}\tcontent: {review['document']}" for review in reviews])
+    output = chain.invoke({"reviews": reviews})
+
+    return output
     
 if __name__ == "__main__":
-    print(inference_all([
+    print(inference_all_langchain([
         {"id": 1, "document": "뭐야 이 평점들은.... 나쁘진 않지만 10점 짜리는 더더욱 아니잖아"},
         {"id": 2, "document": "지루하지는 않은데 완전 막장임"},
         {"id": 3, "document": "3D만 아니었어도 별 다섯개 줬을텐데.."},
