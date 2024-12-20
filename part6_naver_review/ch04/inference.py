9b5d82faf373bdbad83d7a97924f5467d72097cb
jake <epikeran@gmail.com>
2024-12-20 09:43:45 +0900

    feat : Add review summary logic with langchain

--- part6_naver_review/ch04/inference.py
+++ part6_naver_review/ch04/inference.py
@@ -2,9 +2,13 @@ import os
 import json
 
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 from openai import Client
+from pydantic import BaseModel
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
 
-from prompt_template import prompt_template
+from prompt_template import prompt_template, prompt_template_langchain
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -27,8 +31,31 @@ def inference(reviews):
 
     return output_json
 
+class Output(BaseModel):
+    summary: str
+
+output_parser = PydanticOutputParser(pydantic_object=Output)
+prompt_maker = PromptTemplate(
+    template=prompt_template_langchain,
+    input_variables=["reviews"],
+    partial_variables={"format_instructions": output_parser.get_format_instructions()}
+)
+model = ChatOpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model_name="gpt-3.5-turbo",
+    temperature=0.0,
+)
+chain = (prompt_maker | model | output_parser)
+
+def inference_langchain(reviews):
+    reviews = "\n".join(reviews)
+    # prompt = prompt_maker.invoke({"reviews": reviews})
+    output = chain.invoke({"reviews": reviews})
+
+    return output.summary
+
 if __name__ == "__main__":
-    print(inference([
+    print(inference_langchain([
         "정말 재미없네요.",
         "시간 가는줄 모르고 정말 즐겁게 봤습니다.",
         "로다쥬 나오는 영화는 무조건 추천이요",
