1d12f293b816f903a80232752e3e61d55d35d6ed
jake <epikeran@gmail.com>
2024-12-20 14:52:20 +0900

    feat : Add json output format and get cost

--- part9_ai_faq/ch01/prompt_template.py
+++ part9_ai_faq/ch01/prompt_template.py
@@ -3,3 +3,11 @@ prompt_template = """다음 내용을 읽고 FAQ를 만들어 주세요.
 ```
 {product_detail}
 ```"""
+
+prompt_template_json = """다음 내용을 읽고 FAQ를 만들어 주세요.
+
+{format_instruction}
+
+```
+{product_detail}
+```"""
