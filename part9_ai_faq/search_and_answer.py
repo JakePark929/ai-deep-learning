f918f36de4b17ed80ed1d27e903561f4d7b239df
jake <epikeran@gmail.com>
2024-12-20 17:37:39 +0900

    feat : Add fast api server for faq service

--- part9_ai_faq/search_and_answer.py
+++ part9_ai_faq/search_and_answer.py
@@ -34,7 +34,7 @@ A: {context['answer']}"""
     prompt = prompt_template.format(context=context_join, question=question)
 
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-3.5-turbo-0125",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt}
