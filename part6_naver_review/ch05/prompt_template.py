7f57ed58b2cbfdd702b8ecf981d2bffe1bc67052
jake <epikeran@gmail.com>
2024-12-20 10:47:54 +0900

    feat : Add function calling logic in integration service

--- part6_naver_review/ch05/prompt_template.py
+++ part6_naver_review/ch05/prompt_template.py
@@ -1,15 +1,3 @@
-prompt_template = """다음은 영화에 대한 리뷰들입니다.
-1. 영화에 대해 긍정적이면 1, 부정적이면 0으로 평가해주세요.
-2. 리뷰에서 긍정적인 키워드, 부정적인 키워드를 추출해주세요.
-3. 리뷰 내용들을 종합적으로 요약해주세요.
-
-아래 json schema를 참고하여 json 응답을 생성해주세요.
-{json_schema}
-
-```reviews
-{reviews}
-```"""
-
 json_schema = {
   "$schema": "http://json-schema.org/draft-07/schema#",
   "type": "object",
@@ -48,6 +36,29 @@ json_schema = {
   },
   "required": ["reviews", "summary"]
 }
+
+prompt_template = """다음은 영화에 대한 리뷰들입니다.
+1. 영화에 대해 긍정적이면 1, 부정적이면 0으로 평가해주세요.
+2. 리뷰에서 긍정적인 키워드, 부정적인 키워드를 추출해주세요.
+3. 리뷰 내용들을 종합적으로 요약해주세요.
+
+아래 json schema를 참고하여 json 응답을 생성해주세요.
+{json_schema}
+
+```reviews
+{reviews}
+```"""
+
+prompt_template_function_calling = """다음은 영화에 대한 리뷰들입니다.
+1. 영화에 대해 긍정적이면 1, 부정적이면 0으로 평가해주세요.
+2. 리뷰에서 긍정적인 키워드, 부정적인 키워드를 추출해주세요.
+3. 리뷰 내용들을 종합적으로 요약해주세요.
+
+json 으로 응답해 주세요.
+
+```reviews
+{reviews}
+```"""
 
 prompt_template_langchain = """다음은 영화에 대한 리뷰들입니다.
 1. 영화에 대해 긍정적이면 1, 부정적이면 0으로 평가해주세요.
